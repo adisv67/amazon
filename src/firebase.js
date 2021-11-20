403813fb2a0394b506dc0803df4684777c731041
Adesh S <adesh.sonawane@morfsys.com>
2021-11-20 14:45:48 +0530

    initial comment

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBKoVoDOu24f9u9e0kuI_LWwTEB9A6GgOI",
+    authDomain: "challange-1024d.firebaseapp.com",
+    projectId: "challange-1024d",
+    storageBucket: "challange-1024d.appspot.com",
+    messagingSenderId: "21409947515",
+    appId: "1:21409947515:web:70cc58c31e22b2653327de",
+    measurementId: "G-9WJPM16HKV"
+  };
