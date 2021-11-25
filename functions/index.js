1b57b0798f8f602c190210e35605d6584fe0e1ef
Adesh S <adesh.sonawane@morfsys.com>
2021-11-25 05:56:16 +0530

    firebase- functions added

---
+++ functions/index.js
@@ -0,0 +1,6 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const stripe = require("stripe")
+('sk_test_51JyxCCSExlE9MzQ0VIVYXJG1pCJZ3FskiExvpwjXuSLS33zw7MiIgRq3H1s4LR2tppmfZuEHm7dMUBiG8GQzmvc700ojufqT2Z')
+
+
